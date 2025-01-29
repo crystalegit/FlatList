0290c61ddeb34acada16d58b66a70207aa5fff80
crystalegit <145364410+crystalegit@users.noreply.github.com>
2025-01-29 11:02:39 -0600

    learnt how to select items

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -9,8 +9,28 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import { useState } from "react";
 
 export default function Index() {
+    type dataType = {
+        id: string; //unique identifier for each list element
+        title: string; //display
+    };
+
+    const DATA: dataType[] = [
+        { id: "1", title: "first" },
+        { id: "2", title: "sec" },
+        { id: "3", title: "thrird" },
+    ];
+
+    // create func to call on item click. pass the item in as param
+    const selectedListItem = (item: dataType) => {
+        console.log(item.title + " clicked");
+        setSelectedId(item.id);
+    };
+
+    const [selectedId, setSelectedId] = useState<string>("");
+
     return (
         <View style={defaultStyles.container}>
             <View style={defaultStyles.titleContainer}>
@@ -20,7 +40,42 @@ export default function Index() {
             </View>
             <View style={[defaultStyles.textContainer, { flex: 1 }]}>
                 <View style={styles.flatlist}>
-                    <Text>This is where our list will go</Text>
+                    <FlatList
+                        data={DATA}
+                        keyExtractor={(item: dataType) => item.id}
+                        extraData={selectedId}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity
+                                onPress={() => selectedListItem(item)}
+                            >
+                                <View
+                                    style={[
+                                        styles.titleContainer,
+                                        {
+                                            backgroundColor:
+                                                item.id === selectedId
+                                                    ? colors.primary
+                                                    : colors.secondary,
+                                        },
+                                    ]}
+                                >
+                                    <Text
+                                        style={[
+                                            styles.titleText,
+                                            {
+                                                color:
+                                                    item.id === selectedId
+                                                        ? colors.text.light
+                                                        : colors.text.dark,
+                                            },
+                                        ]}
+                                    >
+                                        {item.title}
+                                    </Text>
+                                </View>
+                            </TouchableOpacity>
+                        )}
+                    />
                 </View>
             </View>
         </View>
